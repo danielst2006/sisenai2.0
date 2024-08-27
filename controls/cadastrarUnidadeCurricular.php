17ce7660802a0d804276ac08166dc65a58b89e94
Brunnontj <bruno.r.brito@aluno.senai.br>
2024-08-27 17:21:01 -0300

    Bruno OK

--- controls/cadastrarUnidadeCurricular.php
+++ controls/cadastrarUnidadeCurricular.php
@@ -1,32 +1,47 @@
-<?php 
+<?php
 
 include_once '../bd/conn.php';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+// Verifica se a ação foi enviada pelo formulário
+$action = isset($_POST['action']) ? $_POST['action'] : '';
 
-    $action = $_POST['action'];
-    $id = isset($_POST['idunidade_curricular']) ? $_POST['idunidade_curricular'] : null;
-    $nome_unidade = $_POST['nome_unidade'];
-    $carga_horaria = $_POST['carga_horaria'];
-    $curso_id = $_POST['curso_id'];
+$idunidade_curricular = isset($_POST['idunidade_curricular']) ? $_POST['idunidade_curricular'] : '';
+$nome_unidade = isset($_POST['nome_unidade']) ? $_POST['nome_unidade'] : '';
+$carga_horaria = isset($_POST['carga_horaria']) ? $_POST['carga_horaria'] : '';
+$curso_id = isset($_POST['curso_id']) ? $_POST['curso_id'] : '';
 
-    if ($action == 'add') {
-        $sql = "INSERT INTO unidade_curricular (nome_unidade, carga_horaria, curso_id) VALUES ('$nome_unidade', '$carga_horaria', '$curso_id')";
-    } elseif ($action == 'update') {
-        $sql = "UPDATE unidade_curricular SET nome_unidade='$nome_unidade', carga_horaria='$carga_horaria', curso_id='$curso_id' WHERE idunidade_curricular=$id";
-    } elseif ($action == 'delete') {
-        $sql = "DELETE FROM unidade_curricular WHERE idunidade_curricular=$id";
-    }
+// Inicializa a variável $sql
+$sql = '';
 
-    $resultado = mysqli_query($conn, $sql);
-    if ($resultado) {
+if ($action == 'delete') {
+    // Deletar unidade curricular
+    $sql = "DELETE FROM unidade_curricular WHERE idunidade_curricular = '$idunidade_curricular'";
+} elseif ($idunidade_curricular) {
+    // Editar unidade curricular existente
+    $sql = "UPDATE unidade_curricular 
+            SET nome_unidade = '$nome_unidade', 
+                carga_horaria = '$carga_horaria', 
+                curso_id = '$curso_id' 
+            WHERE idunidade_curricular = '$idunidade_curricular'";
+} else {
+    // Adicionar nova unidade curricular
+    $sql = "INSERT INTO unidade_curricular (nome_unidade, carga_horaria, curso_id) 
+            VALUES ('$nome_unidade', '$carga_horaria', '$curso_id')";
+}
+
+// Executa a consulta SQL
+if ($sql) {
+    if (mysqli_query($conn, $sql)) {
         header('Location: ../form/formUnidadeCurricular.php?status=success');
     } else {
         header('Location: ../form/formUnidadeCurricular.php?status=error');
     }
-    exit();
+} else {
+    // Se a variável $sql estiver vazia, redirecione para a página com uma mensagem de erro
+    header('Location: ../form/formUnidadeCurricular.php?status=error');
 }
 
-mysqli_close($conn)
+// Fecha a conexão
+mysqli_close($conn);
 
 ?>
